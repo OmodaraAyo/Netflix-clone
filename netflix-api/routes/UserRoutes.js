a3073ae5603dbe68b123e3eeb713624f3cb3b23e
OmodaraAyo <ayodeleomodara1234@gmail.com>
2024-12-05 20:42:41 +0100

    committed

---
+++ netflix-api/routes/UserRoutes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+
+router.post("/add, addToLikedMovies");
+
+
+module.exports = router;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "../styles/logo.module.css"
+
+const Logo = ()=> {
+  return (
+    <div className={styles.container}>
+      <img src="/images/Netflix.png" alt="logo"/>
+    </div>
+  )
+}
+export default Logo;
